2d3092a057658134af691e86d4b4062fdead7b76
ilya sobolev <123ilya@gmail.com>
2022-03-18 00:49:16 +1000

    it works with css

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,9 @@
 let articleTitle = document.querySelector("#articleTitle");
 //Получаем элемент статьи
 let article = document.querySelector("#article");
-
+/**Функция получает все статьи списком, выбирает случайную статью,
+ * на основе этой статьи изменяет содержание элементов страницы
+ */
 function randomArticle() {
   fetch("https://jsonplaceholder.typicode.com/posts/")
     .then((response) => response.json())
@@ -12,9 +14,10 @@ function randomArticle() {
       article.innerHTML = randomArticle.body;
     });
 }
+/**Запускаем функцию и получаем изначальную статью на странице */
 randomArticle();
 
 // Получаем кнопку
 let button = document.querySelector("#button");
-
+/**При нажатии кнопки срабатывает функция и статья вновь меняется на случайную */
 button.addEventListener("click", () => randomArticle());
